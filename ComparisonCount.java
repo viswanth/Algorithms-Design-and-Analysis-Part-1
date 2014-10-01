3343d965880f3e6c09492444430a90c4f8ca3a9b
Viswanth Chadalawada <viswanth.chadalawada@gmail.com>
2014-09-30 17:44:02 -0700

    added comments

--- ComparisonCount.java
+++ ComparisonCount.java
@@ -12,12 +12,19 @@ import java.util.List;
 
 public class ComparisonCount {
 
+	#the array used to contain the elements throughout the process of sorting.
 	private int[] unsortedArray;
 	
 	private int arrayLength, comparisonCount = 0;
 
 	private String selection;
 	
+	
+	/**
+     	* Initializes an empty unsorted array with the array provided 
+     	* and send it over to the sort method.
+     	* @param the integer array to be sorted
+     	*/
 	public void quickSort(int[] array){
 		if(array == null || array.length == 0){
 			return;
@@ -28,24 +35,32 @@ public class ComparisonCount {
 		sort(0,arrayLength - 1);
 	}
 	
+	/**
+     	* Sorts the sub array it receives in increasing order.
+     	* @param left the left index of the subarray to be sorted
+     	* @param right the right index of the subarray to be sorted
+     	*/
 	public void sort(int left, int right){
-	
+		
+		//increment the count of comparison in every call to the 
+		//sort method.
 		comparisonCount += right - left;
 
-		
+		//the index of the pivot in the array
 		int pivotIndex = 0;
 		int pivot = 0;
 		
+		//the switch statement decides the position of the pivot
 		switch(selection){
-		case "FIRST":
+		case "FIRST"://the pivot is the first element of the array
 			pivotIndex = left;
 			pivot = getPivot(pivotIndex);
 			break;
-		case "LAST":
+		case "LAST"://the pivot is the last element of the array
 			pivotIndex = right;
 			pivot = getPivot(pivotIndex);
 			break;
-		case "MEDIAN":
+		case "MEDIAN"://the pivot is the median of the elements of the array
 			int first = left, last = right, middle = 0;
 			if((right - left) % 2 == 0){ 
 				middle = first + (last - first)/2;
@@ -53,30 +68,46 @@ public class ComparisonCount {
 				middle = first + (last - first - 1)/2;
 			}
 			
+			//the method to calculate the index of the pivot
 			pivotIndex = getMedian(first,middle,last);
 			pivot = getPivot(pivotIndex);
 			break;
 		}
 		
+		//after calculating the index of the pivot, take it to the front and continue execution
 		if(pivotIndex != left)
 			swap(left,pivotIndex);
 		
 		int i = left + 1;
 		
+		//the logic for sorting
 		for(int j = left + 1; j <= right; j++){
 			if(unsortedArray[j] < pivot){
 				swap(i,j);
 				i++;
 			}
 		}
+		//sort the pivot now to put it at its exact location
+		//all elements lesser than the pivot are now before this position
+		//and all elements greater lie after the pivot position
 		swap(left,i-1);
+		//sort the left half of the unsorted list
 		if(left < i-1)
 			sort(left,i-2);
+		//sort the right half of the unsorted list
 		if(right > i)
 			sort(i,right);
 
 	}
 
+
+	/**
+     	* return the median of the subarray
+     	* @param first the index of the first element in the subarray
+     	* @param middle the index of the middle element in the subarray
+     	* @param last the index of the last element in the subarray
+     	* @return index of the median element in the subarray
+     	*/
 	private int getMedian(int first, int middle, int last) {
 		// TODO Auto-generated method stub
 		
@@ -94,12 +125,22 @@ public class ComparisonCount {
 
 	}
 
+	/**
+     	* returns the pivot element given the index
+     	* @param right the index of the pivot element
+     	* @return pivot element
+     	*/
 	private int getPivot(int right) {
 		// TODO Auto-generated method stub
 		//return unsortedArray[arrayLength - 1];
 		return unsortedArray[right];
 	}
 
+	/**
+     	* swap two elements of the array
+     	* @param i index of the first element
+     	* @param j index of the second element
+     	*/
 	private void swap(int i, int j) {
 		// TODO Auto-generated method stub
 		int temp = unsortedArray[i];
@@ -107,6 +148,11 @@ public class ComparisonCount {
 		unsortedArray[j] = temp;
 	}
 	
+	/**
+     	* get the text file input of integers in the form of an integer array
+     	* @param filename path of the input text file
+     	* @return integer array of the inout file
+     	*/
 	private int[] getArrayFromFile(InputStream fileName)
 	{
 		try
